201d5925ffa1bd98c25b91ecdcf5b76432cfd4fb
Vasanth Prabhakaran <vasanthprabhakaran1999@gmail.com>
2020-11-12 18:41:34 +0530

    App Hosted

--- server/index.js
+++ server/index.js
@@ -15,7 +15,11 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const PORT = process.env.PORT;
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+});
+
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
